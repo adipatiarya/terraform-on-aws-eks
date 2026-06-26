6b9bd4e1008ae5350f4a4cfff61e79c7531d4aab
ratmandev <ratmandev@gmail.com>
2026-06-26 16:02:54 +0000

    update

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c1-versions.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c1-versions.tf
@@ -1,5 +1,17 @@
 # Terraform Block
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.52.0"
+    }
+  }
+}
 
 # Provider Block
 
+provider "aws" {
+  # Configuration options
+  profile = "default"
+  region  = var.aws_region
+}
